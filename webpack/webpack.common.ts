ab2a21121f42a7a4659c31517ab1269ebb9ad83c
Jan Pesa <jan.pesa@gmail.com>
2020-01-15 14:25:28 +0100

    Fix vendor chunk generation for production builds

--- webpack/webpack.common.ts
+++ webpack/webpack.common.ts
@@ -17,8 +17,14 @@ const config: Configuration = {
   },
   optimization: {
     splitChunks: {
-      name: 'vendor',
-      chunks: 'initial',
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          name: 'vendor',
+          test: /node_modules/,
+          enforce: true,
+        },
+      },
     },
   },
   module: {
